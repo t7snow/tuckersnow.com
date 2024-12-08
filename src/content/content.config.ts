b2a9929fade13d1c211a2f8cc79e3eb660d24ff2
t7snow <tuckerwsnow@gmail.com>
2024-12-08 00:39:47 -0500

    setting up recent posts

---
+++ src/content/content.config.ts
@@ -0,0 +1,16 @@
+// import utilities from astro:content
+import { defineCollection, z } from 'astro:content';
+
+// Define a schema for the blogposts collection
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = {
+  blogposts: blog,
+};
