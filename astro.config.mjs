e98c56c6808934e7bd65878c02045b5438a55185
t7snow <tuckerwsnow@gmail.com>
2024-12-18 14:55:49 -0500

    layout complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+
 // https://astro.build/config
 export default defineConfig({
+	redirects: {
+    '/short/linkedin': 'https://www.linkedin.com/in/tucker-snow-8672ab20a/',
+		'/short/github': 'https://github.com/t7snow',
+		'/short/twitter': 'https://x.com/t7snow'
+  },
 	integrations: [tailwind()],
 });
